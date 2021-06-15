ab06bb7517371aec2cc5ce1f4541670cbea959e2
Rui Simao <rui.simao@hostelworld.com>
2021-06-15 18:43:48 +0100

    chore: update nextjs

--- components/sections/about-me.jsx
+++ components/sections/about-me.jsx
@@ -7,21 +7,18 @@ function AboutMe() {
       <section id="about-me" className="pt-2">
         <BasicReveal trigger={<div className="text-left" />}>
           <Highlight className="text-3xl mb-5">About me</Highlight>
-          <div className="grid">
-            <div className="">
-              <p className="mb-2 text-light font-carrois text-lg text-bold">
-                I&apos;m a{" "}
-                <span className="underline-text">Fullstack Developer</span> from
-                Portugal, passionate about building beautiful interactive
-                user-friendly experinces. With 4+ years in the game, frontend is
-                my jam, but I know my way in the backend.
-              </p>
-            </div>
-            <p className="mb-2 text-light font-carrois text-lg text-bold">
-              I&apos;m a fast learner, problem solving enthusiast, always
-              looking forward to improve myself and helper others.
+          <div>
+            <p className="mb-5 text-light font-carrois text-lg text-bold">
+              I&apos;m a Fullstack Developer from Portugal, passionate about
+              building beautiful interactive user-friendly experinces. With 4+
+              years in the game, frontend is my jam, but I know my way in the
+              backend.
             </p>
-            <p className="mb-2  text-light font-carrois text-lg text-bold">
+            <p className="mb-5 text-light font-carrois text-lg text-bold">
+              I&apos;m a, problem solving enthusiast, always looking forward to
+              improve myself and helper others.
+            </p>
+            <p className="text-light font-carrois text-lg text-bold">
               I&apos;m a retired lineman, with the weird tradition of rewatching
               Lord of the Rings every single year. I like to find myself with a
               bodyboard in the nearest beach.
@@ -38,10 +35,10 @@ function AboutMe() {
           position: absolute;
           left: 50%;
           bottom: 0;
-          width: 150px;
+          width: 100%;
           height: 10px;
           transform: skew(-12deg) translateX(-50%);
-          background: var(--color-blue);
+          background: var(--color-dark);
           z-index: -1;
         }
       `}</style>

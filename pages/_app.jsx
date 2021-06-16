f089886a7401307b47242afee657e4eadb02da6e
Rui Simao <rui.simao@hostelworld.com>
2021-06-16 19:11:03 +0100

    feat: google analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,13 +3,28 @@ import './global.css';
 import Head from 'next/head';
 import smoothscroll from 'smoothscroll-polyfill';
 import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
   useEffect(() => {
     if (typeof window !== 'undefined') {
       smoothscroll.polyfill();
     }
   }, []);
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+        page_path: url,
+      });
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <>
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { GA_TRACKING_ID } from '../utils/gtag';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -30,6 +31,20 @@ class MyDocument extends Document {
           <link
             href="https://fonts.googleapis.com/css?family=Roboto&display=swap"
             rel="stylesheet"
+          />
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+          <script
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
           />
         </Head>
         <body>
